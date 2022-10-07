d1cc5a882a1a456452fad1724003d9e98334a46e
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-10-06 22:49:12 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function suma(n1,n2){
+sum=n1+n2;
+}
+function resta(n1,n2){
+    res=n1-n2;
+  }
+
+  let cont=5;
+  let n1=3;
+  let n2=5;
+  let hola=Array;
+  for(i=0;i<cont;i++){
+  let n3=suma(n1,n2);
+  hola[i]=n3; 
+  n2++;
+  n1++;
+   }
